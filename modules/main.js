470c625321dc09e643506c7fbf15e1b673ddbf4c
Aaron Lip <amitchel.lip@gmail.com>
2021-05-16 14:49:41 -0600

    choice debugging

--- modules/main.js
+++ modules/main.js
@@ -5,8 +5,19 @@ let json = await f.text();
 Hooks.on("ready", () => {
     console.log("ink in the foundry");
     var inkStory = new inkjs.Story(json);
+    while (inkStory.canContinue) {
+        step(inkStory);
+    }
+});
+
+function step(inkStory) {
     console.log(inkStory.ContinueMaximally());
     for (var choice of inkStory.currentChoices) {
         console.log(choice.text);
     };
-});
+    if (inkStory.currentChoices.length > 0) {
+        inkStory.ChooseChoiceIndex(0);
+    } else {
+        console.log("THE END");
+    }
+}
