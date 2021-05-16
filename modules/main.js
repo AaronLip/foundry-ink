ae63713e2363026d28af8290e75a408b8e8761b0
Aaron Lip <amitchel.lip@gmail.com>
2021-05-16 14:41:18 -0600

    Choice text now

--- modules/main.js
+++ modules/main.js
@@ -1,10 +1,12 @@
 import '../modules/ink.js';
-let f = await fetch("./modules/foundry-ink/intercept.json");
+let f = await fetch("./modules/foundry-ink/ink-files/intercept.json");
 let json = await f.text();
 
 Hooks.on("ready", () => {
     console.log("ink in the foundry");
     var inkStory = new inkjs.Story(json);
     console.log(inkStory.ContinueMaximally());
-    console.log(inkStory.currentChoices);
+    for (var choice of inkStory.currentChoices) {
+        console.log(choice.text);
+    };
 });
