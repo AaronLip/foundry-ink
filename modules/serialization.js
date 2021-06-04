e560f605a56012d347f80f613c8f39b6e9c49519
Aaron Lip <amitchel.lip@gmail.com>
2021-06-03 21:42:07 -0600

    Serde function serialization tested

--- modules/serialization.js
+++ modules/serialization.js
@@ -79,7 +79,7 @@ export class HookData {
     /**
      * Converts a data object into a {@link serde.HookData}.
      * 
-     * @param {HookData} data - the content to be wrapped by {@link serde.HookData}
+     * @param {Object} data - the content to be wrapped by {@link serde.HookData}
      * @param {String} data.triggerHook - the hook that will run the story's
      *     callback.
      * @param {String} data.hookType - one of 'once', 'when', or 'until'.
@@ -96,8 +96,7 @@ export class HookData {
          * Serialize a javascript function into a string so that it can survive
          * a reload.
          */
-        var bakedFn = ((...args) => this.callbackFn(...args)).toString();
-        this.callbackFn = `'use strict';return (${bakedFn})();`;
+        this.callbackFn = `return (${data.callbackFn.toString()})(...args);`
     }
 
     /**
@@ -122,34 +121,72 @@ export class HookData {
     fromFlag(flaggable) {
         return new this(flaggable.getFlag('foundry-ink', 'hook'));
     }
-
 }
 
 /**
- * Serializes external binding data into a flag for a foundry object. This is
- * necessary because external bindings created by macros or through console
- * commands do not persist when foundry is rebooted or refreshed.
+ * Information about external bindings that is used to serialize them. Ensures
+ * that bindings created by macros, console commands, or callbacks can be
+ * rebound when foundry reboots or refreshes.
  * 
- * @param {FlaggableObject} flaggable - the object that implements foundry's
- *     setFlag interface.
- * @param {BindingData} bindingData - the external binding to be stored in a
- *     flag.
+ * You can write a [Fallback Function](https://github.com/inkle/ink/blob/master/Documentation/RunningYourInk.md#fallbacks-for-external-functions)
+ * so that inky continues to preview your story, or so that you can run the
+ * story without all of its bindings for testing purposes.
+ * 
+ * @property {Object} data - the content to be wrapped by
+ *     {@link serde.BindingData}
+ * @property {String} data.name - the name of the binding in ink by the when
+ *     bound by the EXTERNAL command or called as a function.
+ * @property {SessionData} data.sessionData - information about the session
+ *     that will be used to rebind the external function.
+ * @property {Function} data.externalFn - the binding to be serialized
+ *     (deserializing this is effectively `eval` running at the privilege of a
+ *     macro).
  */
-export function saveExternalBindingToFlag(flaggable, bindingData) {
-    flaggable.setFlag('foundry-ink', 'binding', bindingData);
-}
+export class BindingData {
+    /**
+     * Converts a data object into a {@link serde.BindingData}.
+     * 
+     * @param {Object} data - the content to be wrapped by {@link serde.BindingData}
+     * @param {String} data.name - the name of the binding in ink by the
+     *     when bound by the EXTERNAL command or called as a function.
+     * @param {SessionData} data.sessionData - information about the session
+     *     that will be used to rebind the external function.
+     * @param {Function} data.externalFn - the binding to be serialized
+     *     (deserializing this is effectively `eval` running at the privilege
+     *     of a macro).
+     */
+    constructor(data) {
+        Object.assign(this, data);
 
-/**
- * Retrieves external binding data, allowing ink's FFI external bindings to be
- * reregistered when a story needs to resume. 
- * 
- * @see {@link saveExternalBindingToFlag} creates the flags accessed by this
- *     function
- * @param {FlaggableObject} flaggable - the object that implements foundry's
- *     setFlag interface.
- * @returns {BindingData} bindingData - the external binding stored in a
- *     flag.
- */
-export function loadExternalBindingFromFlag(flaggable, bindingData) {
-    return flaggable.getFlag('foundry-ink', 'binding');
+        /**
+         * Serialize a javascript function into a string so that it can survive
+         * a reload.
+         */
+        this.callbackFn = `return (${data.callbackFn.toString()})(...args);`
+    }
+
+    /**
+     * Serializes external binding data into a flag for a foundry object. This is
+     * necessary because external bindings created by macros or through console
+     * commands do not persist when foundry is rebooted or refreshed.
+     * 
+     * @param {FlaggableObject} flaggable - the object that implements foundry's
+     *     setFlag interface.
+     */
+    toFlag(flaggable) {
+        flaggable.setFlag('foundry-ink', 'binding', this);
+    }
+
+    /**
+     * Retrieves external binding data, allowing ink's FFI external bindings to be
+     * reregistered when a story needs to resume. 
+     * 
+     * @param {FlaggableObject} flaggable - the object that implements foundry's
+     *     setFlag interface.
+     * @returns {BindingData} bindingData - the external binding stored in a
+     *     flag.
+     */
+    fromFlag(flaggable, bindingData) {
+        return new this(flaggable.getFlag('foundry-ink', 'binding'));
+    }
 }
